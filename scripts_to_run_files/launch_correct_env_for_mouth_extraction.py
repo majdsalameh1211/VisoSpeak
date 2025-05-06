0339e17503acf157822be0338393489b68bc01cc
majdsalameh1211 <157279420+majdsalameh1211@users.noreply.github.com>
2025-05-06 15:51:13 +0300

    Delete scripts_to_run_files/launch_correct_env_for_mouth_extraction.py

--- scripts_to_run_files/launch_correct_env_for_mouth_extraction.py
+++
@@ -1,7 +0,0 @@
-# scripts_to_run_files/launch_correct_env.py
-import subprocess
-
-PYTHON_ENV = r"C:\Users\PC\AppData\Local\Programs\Python\Python310\python.exe"
-SCRIPT = r"C:\Users\PC\Desktop\phaseB\VisoSpeak\scripts_to_run_files\run_mouth_extraction.py"
-
-subprocess.run([PYTHON_ENV, SCRIPT])
