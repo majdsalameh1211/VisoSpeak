a0a4c176667176675cdae05e5bc55da5ddc53af3
Majd Salameh <majdsalameh9@gmail.com>
2025-07-30 20:34:44 +0300

    Clear old repository content

--- src/feature_extraction/cnn3d_extractor.py
+++
@@ -1,7 +0,0 @@
-from torchvision.models.video import r3d_18
-import torch.nn as nn
-
-def get_3dcnn_model():
-    model = r3d_18(pretrained=False)
-    model.fc = nn.Linear(model.fc.in_features, 128)  # Optional: adjust output size
-    return model
